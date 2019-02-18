b51f2ae1d2f8a02e9720f8de85cc3c36473c3b7d
ahmedelmemmi <47481359+ahmedelmemmi@users.noreply.github.com>
2019-02-18 21:53:13 +0100

    Add files via upload
    this is my first work with JS

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.getElementById("bolded").onclick=function(){
+    document.getElementById("changed").style.fontWeight = 'bold';
+}
+
+document.getElementById("itaalic").onclick=function(){
+    document.getElementById("changed").style.fontStyle = "italic";
+}
+document.getElementById("underlinee").onclick=function(){
+    document.getElementById("changed").style.textDecoration = "underline";
+}
+document.getElementById("karl").onclick=function(){
+    document.getElementById("changed").style.fontFamily='Karla', sans-serif;
+}
+document.getElementById("lobster").onclick=function(){
+    document.getElementById("changed").style.fontFamily='Lobster', cursive;
+}
+document.getElementById("pacifico").onclick=function(){
+    document.getElementById("changed").style.fontFamily='Pacifico', cursive;
+}
+document.getElementById("sz1").onclick=function(){
+    document.getElementById("changed").style.fontSize="10px";
+}
+document.getElementById("sz2").onclick=function(){
+    document.getElementById("changed").style.fontSize="20px";
+}
+document.getElementById("sz3").onclick=function(){
+    document.getElementById("changed").style.fontSize="30px";
+}
