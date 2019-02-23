0120aeb97dafcf599238887dae93fb12502b688d
ahmedelmemmi <47481359+ahmedelmemmi@users.noreply.github.com>
2019-02-23 16:09:32 +0100

    Update main.js
    bold/unbold button

--- js/main.js
+++ js/main.js
@@ -1,21 +1,45 @@
 document.getElementById("bolded").onclick=function(){
+    if(document.getElementById("changed").style.fontWeight == 'normal'){
     document.getElementById("changed").style.fontWeight = 'bold';
+    } else {
+        document.getElementById("changed").style.fontWeight = 'normal';
+    }
 }
 
 document.getElementById("itaalic").onclick=function(){
+    if(document.getElementById("changed").style.fontStyle == "normal"){
     document.getElementById("changed").style.fontStyle = "italic";
+    } else {
+        document.getElementById("changed").style.fontStyle = "normal";
+    }
 }
 document.getElementById("underlinee").onclick=function(){
+    if(document.getElementById("changed").style.textDecoration == "none"){
     document.getElementById("changed").style.textDecoration = "underline";
+    } else {
+        document.getElementById("changed").style.textDecoration = "none";
+    }
 }
 document.getElementById("karl").onclick=function(){
+    if(document.getElementById("changed").style.fontFamily=='normal'){
     document.getElementById("changed").style.fontFamily='Karla', sans-serif;
+    } else {
+        document.getElementById("changed").style.fontFamily='normal';
+    }
 }
 document.getElementById("lobster").onclick=function(){
+    if(document.getElementById("changed").style.fontFamily=='normal'){
     document.getElementById("changed").style.fontFamily='Lobster', cursive;
+} else {
+    document.getElementById("changed").style.fontFamily='normal';
+    }
 }
 document.getElementById("pacifico").onclick=function(){
+    if(document.getElementById("changed").style.fontFamily=='normal'){
     document.getElementById("changed").style.fontFamily='Pacifico', cursive;
+    } else {
+        document.getElementById("changed").style.fontFamily='normal';
+    }
 }
 document.getElementById("sz1").onclick=function(){
     document.getElementById("changed").style.fontSize="10px";
